e88b5308d9f0caeeb885517cd4b8e0f539f0a4ca
Pardeep <pardeep889@hotmail.com>
2017-07-06 13:15:59 +0530

    working on image

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -4,28 +4,34 @@ class PostController < ApplicationController
   end
 
   def create
-      a=params[:post][:title]
-      b=params[:post][:image]
-      # binding.pry      
-      current_user.posts.create(title: a, image: b)
-      if current_user.token.blank?
-        redirect_to root_path
-      else
-        begin
+    a=params[:post][:title]
+    b=params[:post][:image]
+    
+    current_user.posts.create(title: a, image: b)
+    if current_user.token.blank?
+      redirect_to root_path
+    else
+      begin
+        if !current_user.provider.blank? && current_user.provider == "twitter"
           client = Twitter::REST::Client.new do |config|
             config.consumer_key        = "E6aO6Z1zS5Jy79bLxdkZySX7g"
             config.consumer_secret     = "MFMsRHq51326gFCHlrogMQ8Xvczw7w1tR0qLhlwehI6kYAzq0P"
             config.access_token        = current_user.twitter_token
             config.access_token_secret = current_user.twitter_secret
-          end
+          end  
           client.update(a)
-          # @graph = Koala::Facebook::API.new(current_user.token)
-          # @graph.put_connections("me", "feed", message: a)
+        end
+        
+        if !current_user.provider.blank? && current_user.provider == "facebook"
+          @graph = Koala::Facebook::API.new(current_user.token)
+          @graph.put_connections("me", "feed", message: a)
           redirect_to "/post", notice: "Post successfully."
-        rescue => e
-           redirect_to "/post", notice: e.message
-        end 
-      end
+        end
+
+      rescue => e
+         redirect_to "/post", notice: e.message
+      end 
+    end
 
   end
 
