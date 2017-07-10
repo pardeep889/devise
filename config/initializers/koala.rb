cc5bd398ee13fc777dc166b38efbbf78feedfad7
Pardeep <pardeep889@hotmail.com>
2017-07-10 12:45:43 +0530

    working with fb four

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,8 +1,8 @@
 Koala.configure do |config|
  # config.access_token = MY_TOKEN
   #config.app_access_token = MY_APP_ACCESS_TOKEN
-  config.app_id = "297376220722071"
-  config.app_secret = "6edd77866aba907b1725a8b2b99a1e1c"
+  config.app_id = "769750563202373"
+  config.app_secret = "c1677f008911d444b127fbe0f8c64f3b"
   # See Koala::Configuration for more options, including details on how to send requests through
   # your own proxy servers.
 end
