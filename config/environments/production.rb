5873fb494fdac2203be9b1a4e3392f110f434369
Pardeep <pardeep889@hotmail.com>
2017-07-11 13:06:40 +0530

    Project-done-without-design

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.default_url_options = { host: 'https://auth-app123.herokuapp.com' }
+
   # Do not dump schema after migrations.
     config.active_record.dump_schema_after_migration = false
 
